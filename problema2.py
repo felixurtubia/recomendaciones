da3eb01cf0ed94b39b44ecae529ceb4bf0f256cd
felucian <felipe.chacon.ossa@gmail.com>
2014-09-03 18:34:00 -0400

    Create problema2.py

---
+++ problema2.py
@@ -0,0 +1,24 @@
+from numpy import *
+
+id_movies = dict()
+usuarios = set()
+
+arch = open('movies.dat')
+for movie in arch:
+	movie = movie.strip().split()
+	id_movies[movie[0]] = ' '.join(movie[1:])
+arch.close()
+
+arch = open('u.dat')
+for dato in arch:
+	dato = dato.strip().split()
+	usuarios.add(dato[0])
+arch.close()
+
+matriz = zeros((len(usuarios),len(id_movies)))
+
+arch = open('u.dat')
+for dato in arch:
+	dato = dato.strip().split()
+	matriz[int(dato[0])-1,int(dato[1])-1] = int(dato[2])
+arch.close()
