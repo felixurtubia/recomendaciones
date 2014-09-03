8d14b0467bff432e102223c468033c31717cde81
felixurtubia <felix.urtubia@gmail.com>
2014-09-03 18:53:38 -0400

    borrar datos

--- problema2.py
+++ problema2.py
@@ -22,3 +22,6 @@ for dato in arch:
 	dato = dato.strip().split()
 	matriz[int(dato[0])-1,int(dato[1])-1] = int(dato[2])
 arch.close()
+
+def compararusuario(idcentral):
+	id_vistas= matriz[idcentral,]
